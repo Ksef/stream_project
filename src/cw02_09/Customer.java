2234d5e9268ca6b412f42e25494bbbe0e6ad977f
Ksef <xephosroman@gmail.com>
2021-09-07 22:18:53 +0300

    homework 50%

--- src/cw02_09/Customer.java
+++ src/cw02_09/Customer.java
@@ -1,7 +1,61 @@
 package cw02_09;
 
+import java.util.Objects;
+
 public class Customer {
     private int id;
     private String name;
     private int tier;
+
+    public Customer(int id, String name, int tier) {
+        this.id = id;
+        this.name = name;
+        this.tier = tier;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
+    public void setTier(int tier) {
+        this.tier = tier;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id && tier == customer.tier && Objects.equals(name, customer.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, tier);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tier=" + tier +
+                '}';
+    }
 }
--- src/cw02_09/Main.java
+++ src/cw02_09/Main.java
@@ -2,6 +2,7 @@ package cw02_09;
 
 import jdk.jfr.Category;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
@@ -21,6 +22,23 @@ public class Main {
         products.add(new Product(7, "sword", "toy",120.0));
         products.add(new Product(8, "cubik", "toy",250.0));
 
+        List<Customer> customers= new ArrayList<>();
+        customers.add(new Customer(1,"qwer", 15));
+        customers.add(new Customer(2,"reqw", 20));
+        customers.add(new Customer(3,"wqer", 25));
+        customers.add(new Customer(4,"ewqr", 30));
+
+        List<Order> orders = new ArrayList<>();
+
+        orders.add(new Order(1, "ok", LocalDate.of(2021,9,2), LocalDate.of(2021,9,5), products, customers.get(2)));
+        orders.add(new Order(2, "ok", LocalDate.of(2021,9,2), LocalDate.of(2021,9,6), products, customers.get(3)));
+        orders.add(new Order(3, "ok", LocalDate.of(2021,9,2), LocalDate.of(2021,9,7), products, customers.get(1)));
+        orders.add(new Order(4, "ok", LocalDate.of(2021,9,2), LocalDate.of(2021,9,8), products, customers.get(4)));
+        orders.add(new Order(5, "ok", LocalDate.of(2021,3,14), LocalDate.of(2021,3,16), (List<Product>) products.get(4), customers.get(4)));
+        orders.add(new Order(6, "ok", LocalDate.of(2021,3,14), LocalDate.of(2021,3,18), (List<Product>) products.get(4), customers.get(4)));
+        orders.add(new Order(7, "ok", LocalDate.of(2021,3,14), LocalDate.of(2021,3,17), (List<Product>) products.get(4), customers.get(4)));
+        orders.add(new Order(8, "ok", LocalDate.of(2021,3,14), LocalDate.of(2021,3,19), (List<Product>) products.get(4), customers.get(4)));
+
 //        1. Obtain a list of products belongs to category “Books” with price > 100
         Predicate<Product> more100 = product -> product.getPrice() >= 100;
         products.stream().filter(more100).forEach(product -> System.out.println(product));
@@ -39,8 +57,22 @@ public class Main {
 //        3. Get the cheapest products of “Books” category
         Product product = products.stream()
                 .min((o1, o2) -> (int) (o1.getPrice() - o2.getPrice())).get();
-        System.out.println(product);
+//        .forEach (System.out::println);
+        System.out.println(products);
+        System.out.println("================");
 
+//        4. Get the 3 most recent placed order
+
+        List<Order> collect = orders.stream().sorted((o1, o2) -> o1.getOrderDate().compareTo(o2.getOrderDate())).limit(3).collect(Collectors.toList());
+        System.out.println(collect);
+//        System.out.println(order);
+//        System.out.println(orders);
+////        5. Calculate order average payment placed on 14-Mar-2021 (використайте в процесі mapToDouble)
+//        Order order1 = orders.stream().filter(order -> order.getOrderDate().equals(LocalDate.of(2021,3, 14)))
+//                .mapToDouble(value -> )
+////        6. Get the most expensive product by category (використайте groupingBy)
+////        Product product1 = products.stream();
+////                .collect(products.groupingBy)
 
     }
 }
